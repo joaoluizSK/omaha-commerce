c34724c8555a9cffa6b40a07a6b0109ec720fcee
wesley.fuchter <wesley.fuchter@gmail.com>
2016-10-12 20:27:26 -0300

    Viwes

--- omaha-commerce/static/js/src/app/app.js
+++ omaha-commerce/static/js/src/app/app.js
@@ -2,23 +2,42 @@ var app = angular.module('app', ['ngRoute']);
 
 app.config(function($routeProvider, $locationProvider) {
 
-	$routeProvider.when('/login', {
-		templateUrl : 'login.html',
-		contoller : 'LoginController'
-	});
+    $routeProvider.when('/login', {
+        templateUrl: 'login.html',
+        contoller: 'LoginController'
+    });
 
-	$routeProvider.when('/usuario', {
-		templateUrl : 'usuario.html',
-		contoller : 'UsuarioController',
-		controlerAs : 'uc'
+    $routeProvider.when('/usuario', {
+        templateUrl: 'usuario.html',
+        contoller: 'UsuarioController',
+        controlerAs: 'uc'
+    });
 
-	});
+    $routeProvider.when('/produto', {
+        templateUrl: 'produto.html',
+        contoller: 'ProdutoController',
+        controlerAs: 'pc'
+    });
 
-	$routeProvider.when('/loja', {
-		templateUrl : 'loja.html',
-		contoller : 'LojaController'
-	});
+    $routeProvider.when('/categoria', {
+        templateUrl: 'categoria.html',
+        contoller: 'CategoriaController',
+        controlerAs: 'cc'
+    });
 
-	$routeProvider.otherwise ({ redirectTo: '/' });
+    $routeProvider.when('/venda', {
+        templateUrl: 'venda.html',
+        contoller: 'VendaController',
+        controlerAs: 'vc'
+    });
+
+    $routeProvider.when('/loja', {
+        templateUrl: 'loja.html',
+        contoller: 'LojaController'
+    });
+
+    $routeProvider.otherwise({
+        redirectTo: '/'
+    });
 
 });
--- omaha-commerce/static/js/src/controllers/index.controller.js
+++ omaha-commerce/static/js/src/controllers/index.controller.js
@@ -1,9 +1,12 @@
 app.controller('IndexController', IndexController);
 
-IndexController.$inject = ['$scope','IndexService', '$location'];
+IndexController.$inject = ['$scope', 'IndexService', '$location'];
 
 function IndexController($scope, indexService, $location) {
 
-//  $scope.usuario = {nome:'Wesley Fuchter',tipo:'A'};
+    $scope.usuario = {
+        nome: 'Wesley Fuchter',
+        tipo: 'A'
+    };
 
 };
